fec2fe352f37d16ca4a5b56ead84d1f265f2adb9
MichaelHegner <michael.hegner@hemisoft.net>
2017-02-06 18:01:54 +0100

    ImmoRoi: Plane Spring Project

---
+++ src/main/java/ch/hemisoft/immoroi/ImmoRoiApplication.java
@@ -0,0 +1,12 @@
+package ch.hemisoft.immoroi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmoRoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmoRoiApplication.class, args);
+	}
+}
